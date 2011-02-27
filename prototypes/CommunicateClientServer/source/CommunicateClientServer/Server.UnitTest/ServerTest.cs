84c8dc8645ba439e9af9be99162ae31af2c0a9f5
jiaz <jiaji.zh@gmail.com>
2011-02-28 01:06:40 +0800

    Added unit test for Server

---
+++ prototypes/CommunicateClientServer/source/CommunicateClientServer/Server.UnitTest/ServerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace Server.UnitTest
+{
+    [TestClass]
+    public class ServerTest
+    {
+        [TestMethod]
+        public void TestServerListening()
+        {
+            Thread thServer = new Thread(
+                () =>
+                {
+                    StreamServer.Instance.Run();
+                }
+                );
+            Thread client = new Thread(
+                () =>
+                {
+                    TcpClient c = new TcpClient("localhost", 826);
+                    NetworkStream stream = c.GetStream();
+                    stream.WriteByte(10);
+                    stream.Dispose();
+                }
+            );
+            thServer.Start();
+            client.Start();
+            client.Join();
+            thServer.Abort();
+        }
+    }
+}
